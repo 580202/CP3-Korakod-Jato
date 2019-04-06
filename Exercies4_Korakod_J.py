e66acf05c2e56f6e46876df04f55e66e4c54acc8
580202 <shimasa_sonic@hotmail.com>
2019-04-06 17:59:12 +0700

    Update Exercies4_Korakod_J.py

--- Exercies4_Korakod_J.py
+++ Exercies4_Korakod_J.py
@@ -2,7 +2,7 @@ FE = 68.8
 GB = 70.2
 IC = 70.5
 CP = 78.0
-print("------Your_Score------")
+print("---------------Your_Score---------------")
 print("Foundation English               :",   FE)
 print("General Business                 :",   GB)
 print("Introduction to Computer System  :",   IC)
